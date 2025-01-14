99218995c27ff1fe35198ac2633525d6ea568813
Quangtu131 <quangtu6qt@gmail.com>
2025-01-14 14:36:34 +0700

    s

--- ShoppingLaptop_QT/Models/ProductModel.cs
+++ ShoppingLaptop_QT/Models/ProductModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ShoppingLaptop_QT.Models
 {
@@ -10,10 +11,17 @@ namespace ShoppingLaptop_QT.Models
 		[Required, MinLength(4, ErrorMessage = "Yêu cầu nhập tên Sản phẩm")]
 		public string Name { get; set; }
 		public string Slug { get; set; }
+
+
 		[Required, MinLength(4, ErrorMessage = "Yêu cầu nhập mô tả Sản phẩm")]
 		public string Description { get; set; }
+
+
 		[Required, MinLength(4, ErrorMessage = "Yêu cầu nhập giá Sản phẩm")]
+		[Column(TypeName = "decimal(18,2)")]
 		public decimal Price { get; set; }
+
+		public string Image {  get; set; }
 		public int BrandID { get; set; }
 		public int CategoryID { get; set; }
 
